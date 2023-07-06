340edb42e543c41b4470e4275c97c17022c751db
Juan Antonio Ayala Garcia <jag0013@alu.medac.es>
2023-07-06 14:32:08 +0200

    ejercicios_dia_1

---
+++ DIA_1/ejercicios.js
@@ -0,0 +1,53 @@
+// 1.- Escriba un comentario de una sola línea que diga, los comentarios pueden hacer que el código sea legible
+    // los comentarios pueden hacer que el código sea legible
+
+// 2.- Escriba otro comentario individual que diga: Bienvenido a 30DaysOfJavaScript
+    // Bienvenido a 30DaysOfJavascript
+
+// 3.- Escriba un comentario de varias líneas que diga que los comentarios pueden hacer que el código sea legible, fácil de reutilizar e informativo.
+    /* los comentarios pueden hacer que el código sea:
+        legible
+        facil de reutilizar 
+        informartivo 
+    */
+
+// 4.- Cree un archivo variable.js y declare variables y asigne tipos de datos de cadena, booleanos, indefinidos y nulos
+    var string = "Juan Antonio";
+    var boolean = 123456;
+    var nulo = null;
+    var indefinido;
+
+// 5.- use el operador typeof de JavaScript para verificar diferentes tipos de datos. Comprobar el tipo de datos de cada variable
+    console.log (typeof (string));
+    console.log (typeof (boolean));    
+    console.log (typeof (nulo));
+    console.log (typeof (indefinido));
+
+// 6.- Declarar cuatro variables sin asignar valores
+    var nombre;
+    var edad;
+    var dni;
+    var correo;
+
+// 7.- Declarar cuatro variables con valores asignados
+    var nombre = "Juan Antonio"
+    var edad = 23;
+    var dni = "30277963G";
+    var correo = "Juananayala7@gmail.com";
+
+// 8.- Declare variables para almacenar su nombre, apellido, estado civil, país y edad en varias líneas
+    var nombre = "Juan Antonio"
+    var edad = 23;
+    var apellidos = "Ayala Garcia";
+    var país = "España";
+    var estado_civil = "Soltero";
+
+// 9.- Declare variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea
+    var nombre = "Juan Antonio" , edad = 23 , apellidos = "Ayala Garcia" , país = "España" , estado_civil = "Soltero";
+
+// 10.- Declare dos variables myAge y yourAge y asígneles valores iniciales e inicie sesión en la consola del navegador.
+    var myAge = 23;
+    var yourAge = 48;
+    console.log(myAge , yourAge);
+
+ 
